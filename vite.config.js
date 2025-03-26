c5957b46bbb0ac00afffb7d000da4fe203adc3d1
ju4nch11 <Juanchilopezpachao7@gmail.com>
2025-03-26 02:20:08 -0300

    Add vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+export default {
+    server: {
+      allowedHosts: [
+        '8df43820-6768-4214-817e-1e438566bab6-00-1m9bx1lvbyaz3.spock.replit.dev' // Reemplaza con tu host
+      ]
+    }
+  }
